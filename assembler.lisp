012b6d29d7e39bd90e8af43171993be9a6697d08
Sergi Reyner <sergi.reyner@gmail.com>
2018-06-01 23:38:35 +0100

    The assembler now tracks labels (keywords)

--- assembler.lisp
+++ assembler.lisp
@@ -4,24 +4,33 @@
 
 (defmacro assemble-operand (instructions)
   `(let ((arg (car ,instructions)))
+     (when (keywordp arg)
+       (setf arg (gethash arg *labels*)))
      (setf ,instructions (cdr ,instructions))
      (if (< arg 256)
          (list arg)
          (list (shr arg 8 8) (logand #xff arg)))))
 
-
 (defun assemble% (instructions)
-  (loop :for mnemonic := (car instructions)
+  (loop :with *labels* := (make-hash-table)
+     :for mnemonic := (car instructions)
      :while mnemonic
 
-     :for opcode := (position mnemonic *opcode-names*)
-     :collect (prog1 opcode (setf instructions (cdr instructions)))
+     :for opcode := (or (position mnemonic *opcode-names*) -1)
+     :if (keywordp mnemonic)
+     :do (setf instructions (cdr instructions)
+               (gethash mnemonic *labels*) (length bytes))
+     :else
+     :collect (prog1 opcode (setf instructions (cdr instructions))) :into bytes
+     :end
 
      :when (< 1 opcode 32)
-     :append (assemble-operand instructions)
+     :append (assemble-operand instructions) :into bytes
 
      :when (< 15 opcode)
-     :append (assemble-operand instructions)))
+     :append (assemble-operand instructions) :into bytes
+
+     :finally (return bytes)))
 
 (defmacro asm (&rest args)
   `(assemble% ',args))
