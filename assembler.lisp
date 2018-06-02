fe590a0f380c3f03baaecd344f03aea831dd78d2
Sergi Reyner <sergi.reyner@gmail.com>
2018-06-02 01:09:53 +0100

    Added encoding of the operand for word operands
    Execution, however, seems to pose some problems. Numbers may need a
    closer look :-)

--- assembler.lisp
+++ assembler.lisp
@@ -9,7 +9,9 @@
      (setf ,instructions (cdr ,instructions))
      (if (< arg 256)
          (list arg)
-         (list (shr arg 8 8) (logand #xff arg)))))
+         (progn ;; modify opcode if necessary
+           (setf instr (cons (logior (car instr) #b10000000) (cdr instr)))
+           (list (shr arg 8 8) (logand #xff arg))))))
 
 (defun assemble% (instructions)
   (loop :with *labels* := (make-hash-table)
@@ -17,18 +19,26 @@
      :while mnemonic
 
      :for opcode := (or (position mnemonic *opcode-names*) -1)
+     :for instr := (list)
+
      :if (keywordp mnemonic)
-     :do (setf instructions (cdr instructions)
-               (gethash mnemonic *labels*) (length bytes))
+     :do (setf (gethash mnemonic *labels*) (length bytes))
      :else
-     :collect (prog1 opcode (setf instructions (cdr instructions))) :into bytes
+     ;; :collect opcode :into bytes
+     :do (push opcode instr)
      :end
 
+     :do (setf instructions (cdr instructions))
+
      :when (< 1 opcode 32)
-     :append (assemble-operand instructions) :into bytes
+     ;; :append (assemble-operand instructions) :into bytes
+     :do (nconc instr (assemble-operand instructions))
 
      :when (< 15 opcode)
-     :append (assemble-operand instructions) :into bytes
+     ;; :append (assemble-operand instructions) :into bytes
+     :do (nconc instr (assemble-operand instructions))
+
+     :append instr :into bytes
 
      :finally (return bytes)))
 
